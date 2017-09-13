bddee443146763c77474f7358b622ab4a62cd1c1
zsq <597720334@qq.com>
2017-09-13 11:20:02 +0800

    实现课程编辑1的数据回显

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -1,1 +1,25 @@
-console.log(123);
+require('../common/header.js');
+require('../common/aside.js');
+var util = require('../common/util.js');
+
+
+
+/**
+ * 功能点：
+ * 1、数据回显
+ * 2、学科二级联动
+ * 3、表单提交
+ * */
+var cs_id = util.getSearch('cs_id');
+
+/**
+ * 数据回显：
+ * 1、拿到location.search里面的cs_id
+ * 2、通过这个id请求接口获取数据
+ * 3、得到数据渲染后的模版，插入页面指定的位置
+ * */
+$.get('/v6/course/basic', { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        $('#course-edit1').append(template('course-edit1-tpl', data.result));
+    }
+});
